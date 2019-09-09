4a075125254993342d931792821e2aa663088c9f
Mohamed Wael <Mohamed.abdelmaksoud@vodafone.com>
2019-09-09 18:19:32 +0200

    adding constants

---
+++ app/src/main/java/com/github/mohamedwael/latestnews/applevel/Constants.kt
@@ -0,0 +1,6 @@
+package com.github.mohamedwael.latestnews.applevel
+
+const val BASE_API = "https://newsapi.org/v2/"
+
+const val EVEY_THING= "https://newsapi.org/v2/everything?q=all&from=2019-08-09&sortBy=publishedAt&apiKey=8b118e8e691c4882a4c58948efab0848"
+
